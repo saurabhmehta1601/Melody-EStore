04748cb43d0a24c99f1b9dd68c035cc0885aea87
saurabh <saurabhmehta1601@gmail.com>
2023-01-13 01:03:36 +0530

    feat: add compoents

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,11 @@ export default function Home() {
 
       HeroBanner
 
-      <div>
+      <div className='products-heading'>
         <h2>Best selling products</h2>
         <p> Speakers of many variations</p>
       </div>
-      <div>
+      <div className='products-container'>
         {['Product 1', 'Product 2'].map((product) => (product))}
       </div>
 
