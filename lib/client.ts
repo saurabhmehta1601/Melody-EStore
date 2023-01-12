8645a218b8722b0c47ffdadfe63f46460997a6c7
saurabh <saurabhmehta1601@gmail.com>
2023-01-13 01:25:12 +0530

    feat: setup sanity client to access sanity backend

---
+++ lib/client.ts
@@ -0,0 +1,15 @@
+import sanityClient from "@sanity/client";
+import imageUrlBuilder from "@sanity/image-url";
+import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+
+export const client = sanityClient({
+  projectId: "3b14humg",
+  dataset: "production",
+  apiVersion: "2023-01-13",
+  useCdn: true,
+  token: process.env.NEXT_PUBLIC_SANITY_TOKEN,
+});
+
+const builder = imageUrlBuilder(client);
+
+export const urlFor = (source: SanityImageSource) => builder.image(source);
