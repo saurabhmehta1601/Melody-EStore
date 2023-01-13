445300e6adbb6b59e39f016978815aeafbf103b1
saurabh <saurabhmehta1601@gmail.com>
2023-01-13 14:17:21 +0530

    feat: complete Product component

--- components/Product.tsx
+++ components/Product.tsx
@@ -1,8 +1,29 @@
+import { IProduct } from 'app-types'
+import Link from 'next/link'
 import React from 'react'
+import { urlFor } from '../lib/client'
 
-const Product = () => {
+interface IProps {
+    product: IProduct
+}
+
+const Product = ({ product }: IProps) => {
     return (
-        <div>Product</div>
+        <div><Link href={`/product/${product.slug.current}`}>
+            <div className="product-card">
+                <img
+                    className='product-image'
+                    src={urlFor(product.image && product.image[0]) as any}
+                    alt=""
+                    width={250}
+                    height={250}
+                />
+                <p className="product-name">
+                    {product.name}
+                </p>
+                <p className="product-price"> &#8377; {product.price} </p>
+            </div>
+        </Link></div>
     )
 }
 
