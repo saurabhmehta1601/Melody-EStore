7be3577cd6884e192361e5016fdf2059eb0d32c2
saurabh <saurabhmehta1601@gmail.com>
2023-01-14 00:51:10 +0530

    fix: add Toaster context to App

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../components/exports'
 import { StateContext } from 'context/StateContext'
+import { Toaster } from 'react-hot-toast'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <StateContext>
     <Layout>
+      <Toaster />
       <Component {...pageProps} />
     </Layout>
   </StateContext>
