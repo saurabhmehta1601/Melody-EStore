a3f889553a678d927d87ffb832201b1dd9d38f86
saurabh <saurabhmehta1601@gmail.com>
2023-01-14 00:43:03 +0530

    feat: setup context api

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,12 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Layout } from '../components/exports'
+import { StateContext } from 'context/StateContext'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>
+  return <StateContext>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  </StateContext>
 }
