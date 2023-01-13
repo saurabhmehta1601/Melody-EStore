abe1295573ee022f6ad2bfa35476045580628521
saurabh <saurabhmehta1601@gmail.com>
2023-01-13 14:14:19 +0530

    feat: complete component FooterBanner

--- components/FooterBanner.tsx
+++ components/FooterBanner.tsx
@@ -1,8 +1,34 @@
+import { IBanner } from 'app-types'
 import React from 'react'
+import Link from 'next/link'
+import { urlFor } from '../lib/client'
 
-const FooterBanner = () => {
+interface IProps {
+    footerBanner: IBanner
+}
+
+const FooterBanner = ({ footerBanner }: IProps) => {
     return (
-        <div>FooterBanner</div>
+        <div className='footer-banner-container'>
+            <div className="banner-desc">
+                <div className="left">
+                    <p>{footerBanner.discount}</p>
+                    <h3>{footerBanner.largeText1}</h3>
+                    <h3>{footerBanner.largeText2}</h3>
+                    <p>{footerBanner.saleTime}</p>
+                </div>
+                <div className="right">
+                    <p>{footerBanner.smallText}</p>
+                    <h3>{footerBanner.midText}</h3>
+                    <p>{footerBanner.desc}</p>
+                    <Link href={`/product/${footerBanner.product}`}>
+                        <button type="button">{footerBanner.buttonText}</button>
+                    </Link>
+                </div>
+                {/* @ts-expect-error */}
+                <img src={urlFor(footerBanner.image)} alt="" className="footer-banner-image" />
+            </div>
+        </div>
     )
 }
 
