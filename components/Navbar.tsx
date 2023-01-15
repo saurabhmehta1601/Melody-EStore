a5bcf47307f8c951c9f789c263aff1506cac8944
saurabh <saurabhmehta1601@gmail.com>
2023-01-16 01:05:27 +0530

    fix: show product quantity in Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,17 +1,25 @@
-import Link from 'next/link'
-import React from 'react'
-import { AiOutlineShopping } from "react-icons/ai"
+import React, { useContext } from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai'
+
+import Cart from './Cart';
+import { useStateContext } from '../context/StateContext';
 
 const Navbar = () => {
+  const { showCart, setShowCart, qty } = useStateContext();
+
   return (
-    <div className='navbar-container'>
-      <p className='logo'>
-        <Link href="/">EStore</Link>
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Headphones</Link>
       </p>
-      <button type="button" className="cart-icon" onClick={() => { }}>
+
+      <button type="button" className="cart-icon" onClick={() => setShowCart(true)}>
         <AiOutlineShopping />
-        <span className="cart-item-qty">1</span>
+        <span className="cart-item-qty">{qty}</span>
       </button>
+
+      {showCart && <Cart />}
     </div>
   )
 }
