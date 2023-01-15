eb01628694cb79752d5078af9e6f96c310108629
saurabh <saurabhmehta1601@gmail.com>
2023-01-16 01:45:04 +0530

    feat: setup redux toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,11 +4,17 @@ import { Layout } from '../components/exports'
 import { StateContext } from 'context/StateContext'
 import { Toaster } from 'react-hot-toast'
 
+import '@stripe/stripe-js';
+import { Provider } from 'react-redux'
+import { store } from 'redux/store'
+
 export default function App({ Component, pageProps }: AppProps) {
   return <StateContext>
-    <Layout>
-      <Toaster />
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   </StateContext>
 }
