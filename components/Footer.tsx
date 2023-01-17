4d797de6dbcd118e39c06d75b18b9e573d6df8eb
saurabh <saurabhmehta1601@gmail.com>
2023-01-17 22:34:08 +0530

    refactor: update project name in package and title

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
     return (
         <div className="footer-container">
-            <p>Copyright &copy; 2023 All right reserved.</p>
+            <p>Copyright &copy; Melody Store 2023 All right reserved.</p>
         </div>
     )
 }
